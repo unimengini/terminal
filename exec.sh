190d20d11c399ecd4d43785456282d4c71912fcf
unimengini <unimengini@protonmail.com>
2022-04-12 12:12:52 +0530

    Auto Server Commit 12.04.2022 um 12:12

--- exec.sh
+++ exec.sh
@@ -2,10 +2,10 @@
 mkdir /home/user/Desktop/tmp
 cd /home/user/Desktop/tmp 
 date >> time.txt
+git pull origin master
 git add *
 timestamp(){
    date +"%d.%m.%Y um %H:%M"
 }
-git pull origin master
 git commit -am "Auto Server Commit $(timestamp)"
 git push --set-upstream https://unimengini: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@github.com/unimengini/terminal.git master
